537fc88527fe769262e1f045cdf4bcbc350453d9
e155809 <bdubovski@amat.com>
2026-03-23 23:56:46 +0200

    Bump version to 0.2.3

--- flexium/__init__.py
+++ flexium/__init__.py
@@ -39,7 +39,7 @@ from __future__ import annotations
 import atexit
 from typing import Optional
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 # Track if init() has been called
 _initialized = False
